03b00a5ac23d4fdf3389cfd9e9137e9f0cafb13b
codejoes <josephscodes@gmail.com>
2022-10-11 22:18:31 -0400

    added js function and notes to application

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
-var topic = ["HTML", "CSS", "Git", "JavaScript"];
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for(let i = 0; i<topic.length; i++) {
-    console.log(topic[i]);
+function listTopics() {
+for(let i = 0; i<topics.length; i++) {
+    console.log(topics[i]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
+function selectTopic() {
+if (randomTopic === 'HTML') {
  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
  console.log("Let's study JavaScript!");
 } else {
  console.log('Please try again!');
+    }
 }
-*/
+
+console.log("Here are the topics we learned through Prework: ");
+listTopics();
+console.log("Which topic should we study first?" );
+selectTopic();
